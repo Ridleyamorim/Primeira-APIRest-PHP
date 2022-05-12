f147334614c92f5c6bcbccda7f49a4fa005270b1
Ridley <90113835+Ridleyamorim@users.noreply.github.com>
2022-05-12 20:37:08 -0300

    Create polygons.sql
    Adicionar arquivo do banco de dados Mysql para o repositório.

---
+++ polygons.sql
@@ -0,0 +1,43 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Tempo de geração: 12-Maio-2022 às 23:35
+-- Versão do servidor: 5.7.36
+-- versão do PHP: 7.4.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `geometry`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `polygons`
+--
+
+DROP TABLE IF EXISTS `polygons`;
+CREATE TABLE IF NOT EXISTS `polygons` (
+  `id` smallint(6) NOT NULL AUTO_INCREMENT,
+  `type1` varchar(50) DEFAULT NULL,
+  `height` smallint(6) DEFAULT NULL,
+  `base` smallint(6) DEFAULT NULL,
+  `area` smallint(6) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
